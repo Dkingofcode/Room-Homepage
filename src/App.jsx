7253fba2ee45eec3fd8ced48c6433585ec3bc38a
David <doladepo128@gmail.com>
2023-09-01 19:38:27 +0100

    Designed the layout of the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import image1 from "./assets/desktop-image-hero-1.jpg";
+import image2 from "./assets/desktop-image-hero-2.jpg";
+import image3 from "./assets/desktop-image-hero-1.jpg";
+import aboutdark from "./assets/image-about-dark.jpg";
+import aboutlight from './assets/image-about-light.jpg';
+
+let screens = [
+  {image1},
+  {image2},
+  {image3}
+]
+
+let articleheaders = [
+  "Discover innovative ways to decorate",
+  "We are available all across the globe",
+  "Manufactured with the best materials"
+]
+
+let articleText = [
+  "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+  "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest to you using our store locator. Any questions? Don't hesitate to contact us today.",
+  "Our modern furnoture store provides a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+
+]
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [screen, setScreen] = useState(screens);
+ const [header, setHeader] = useState(articleheaders);
+ const [text, setText] = useState(articleText);  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className='main'>
+      
+       <section className='hero'>
+       <nav className='navbar'>
+       <h4>room</h4> 
+       <ul>
+        <li>home</li>
+        <li>shop</li>
+        <li>about</li>
+        <li>contact</li>
+       </ul>
+       </nav>        
+      
+        <img src={image1} className='heroimage' alt="hero-image" />
+        
+        <div className='hero-article'>
+        <h1>Discover innovative ways to decorate</h1>
+        <p>We provide unmatched quality, comfort, and  
+          style for property owner's across the country.
+          Our experts combine form and function in bringing
+          yoour vision to life. Create a room in your own style with our collection
+          and make your property a reflection of you and what you have.
         </p>
+        <span>shop now</span>
+        
+        <button>
+         <img  />
+         <img  />
+        </button>
+        </div>
+
+       </section>
+
+      
+
+        <section className='About'>
+          <img src={aboutdark} className='about-image' alt="about image" />
+
+          <div className='about-text'>
+            <h1>About our Furniture</h1>
+            <p>Our multifunctional collection blends design and function to suit your inidividual
+              taste. Make each room unique, or pick a cohesive theme that best expresses your interests
+              and what inspires you. Find the furniture pieces you need, from traditional to contemporary
+              styles or anything in between. Product specialists are available to help you create your dream space. 
+            </p>
+          </div>
+
+          <img src={aboutlight} className='about-image' alt="about image" />
+        </section>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+              
     </>
   )
 }
 
-export default App
+export default App;
